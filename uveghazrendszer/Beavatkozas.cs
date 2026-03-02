ced98e7c7ea56a3b8f1c38b21318b0c76706ae48
03balint <gyarfas.balint@petrik.hu>
2026-03-02 18:05:32 +0100

    beavatkozas osztaly kesz

--- uveghazrendszer/Beavatkozas.cs
+++ uveghazrendszer/Beavatkozas.cs
@@ -8,5 +8,30 @@ namespace uveghazrendszer
 {
 	internal class Beavatkozas
 	{
-	}
+		string tipus;
+		DateTime datum;
+		Cella celCella;
+		string megjegyzes;
+
+        public Beavatkozas(string tipus, DateTime datum, Cella celCella, string megjegyzes)
+        {
+            this.tipus = tipus;
+            this.datum = datum;
+            this.celCella = celCella;
+            this.megjegyzes = megjegyzes;
+        }
+
+        public string Tipus { get => tipus; set => tipus = value; }
+        public DateTime Datum { get => datum; set => datum = value; }
+        public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
+        internal Cella CelCella { get => celCella; set => celCella = value; }
+
+        public void Kiir()
+        {
+            Console.WriteLine($"Beavatkozás típusa: {tipus}");
+            Console.WriteLine($"Dátum: {datum}");
+            Console.WriteLine($"Cella: ({celCella.Poz})");
+            Console.WriteLine($"Megjegyzés: {megjegyzes}");
+        }
+    }
 }
